4dde05f2cb872f5afb04389d0ba4cb1b1db040ac
BaeJia <qowldk1030@gmail.com>
2025-02-21 15:26:00 +0900

    feat: 이벤트로 alert 추가 (App.js)

--- src/App.js
+++ src/App.js
@@ -5,7 +5,15 @@ function Header(props) {
   return (
     <header className="App-header">
       <h1>
-        <a href="/">{props.title}</a>
+        <a
+          href="/"
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode();
+          }}
+        >
+          {props.title}
+        </a>
       </h1>
     </header>
   );
@@ -17,7 +25,16 @@ function Nav(props) {
     let t = props.topics[i];
     lis.push(
       <li key={t.id}>
-        <a href={"/read/" + t.id}>{t.title}</a>
+        <a
+          id={t.id}
+          href={"/read/" + t.id}
+          onClick={(event) => {
+            event.preventDefault();
+            props.onChangeMode(event.target.id);
+          }}
+        >
+          {t.title}
+        </a>
       </li>
     );
   }
@@ -45,8 +62,18 @@ function App() {
   ];
   return (
     <div className="App">
-      <Header title="REACT"></Header>
-      <Nav topics={topics}></Nav>
+      <Header
+        title="REACT"
+        onChangeMode={() => {
+          alert("Header");
+        }}
+      ></Header>
+      <Nav
+        topics={topics}
+        onChangeMode={(id) => {
+          alert(id);
+        }}
+      ></Nav>
       <Article title="Welcome" body="Hello, WEB"></Article>
       <Article title="again" body="Hello, WEB"></Article>
     </div>
